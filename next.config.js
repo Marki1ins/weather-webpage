2c2b9b68c760a59d63997a65c7084a3541398d5e
Marki1ins <marcosdfcarvalhoo@gmail.com>
2023-07-18 12:19:40 -0300

    feat: adding new settings

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["flagsapi.com", "openweathermap.org"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
